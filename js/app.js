9f6fbdcbc9c395c42db92d46c4ad85691f538d87
Carlos <corrego@damasa.com.co>
2024-08-27 10:01:45 -0500

    modificacion estilos

--- js/app.js
+++ js/app.js
@@ -4,17 +4,42 @@ const btCopiar = document.getElementById("btCopiar");
 const btPegar = document.getElementById("btPegar");
 const txProcesar = document.getElementById("procesarTexto");
 const txResultado = document.getElementById("resultado");
+const lbTitulo = document.getElementById("tituloResultado");
 
 //Evento que se desencadena al dar clic sobre el boton encriptar
 btEncriptar.addEventListener("click",()=>{
     //Asignar valor y replazar caracteres
-    document.getElementById("resultado").value = document.getElementById("procesarTexto").value.toLowerCase().replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");
+    if(txProcesar.value != ""){
+        txResultado.value = txProcesar.value.toLowerCase().replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");
+        lbTitulo.textContent = "Texto encriptado";
+    }else{
+        lbTitulo.textContent = "No hay ningun texto para encriptar";
+        lbTitulo.style.backgroundColor = "red";
+        lbTitulo.style.color = "white";
+        setTimeout(()=>{
+            lbTitulo.textContent = "Resultado de texto encriptado o desencriptado";
+            lbTitulo.style.backgroundColor = "white";
+            lbTitulo.style.color = "black";
+        },2000)
+    }
 })
 
 //Evento que se desencadena al dar clic sobre el boton desencriptar
 btDesencriptar.addEventListener("click",()=>{
     //Desencriptar caracteres
-    document.getElementById("resultado").value = document.getElementById("procesarTexto").value.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
+    if(txProcesar.value != ""){
+        txResultado.value = txProcesar.value.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
+        lbTitulo.textContent = "Texto desencriptado";
+    }else{
+        lbTitulo.textContent = "No hay ningun texto para desencriptar";
+        lbTitulo.style.backgroundColor = "red";
+        lbTitulo.style.color = "white";
+        setTimeout(()=>{
+            lbTitulo.textContent = "Resultado de texto encriptado o desencriptado";
+            lbTitulo.style.backgroundColor = "white";
+            lbTitulo.style.color = "black";
+        },2000)
+    }
 })
 
 
